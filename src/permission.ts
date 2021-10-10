8b9bc85b3c17c7752aa81545e27f91287d806bf1
sxr <sxr@data4truth.com>
2021-10-10 11:43:48 +0800

    feat(登录): 登录时不加/login会报错

--- src/permission.ts
+++ src/permission.ts
@@ -54,13 +54,16 @@ router.beforeEach(async (to, _from, next) => {
     }
   } else {
     /* has no token*/
-
     if (whiteList.indexOf(to.path) !== -1) {
       // in the free login whitelist, go directly
       next()
     } else {
       // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
+      if (to.path === '/404') {
+        next('/login?redirect=/dashboard')
+      } else {
+        next(`/login?redirect=${to.path}`)
+      }
       NProgress.done()
     }
   }
