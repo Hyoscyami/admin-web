31d70bc10c15f4fcc06af3963a8a2ca3275e198f
sxr <sxr@data4truth.com>
2021-10-17 15:09:08 +0800

    feat(文档管理):分页问题

--- src/views/file-management/index.vue
+++ src/views/file-management/index.vue
@@ -67,8 +67,8 @@
     <pagination
         v-show="table.total>0"
         :total="table.total"
-        :page.sync="table.listQuery.page"
-        :limit.sync="table.listQuery.size"
+        v-model:page="table.listQuery.page"
+        v-model:limit="table.listQuery.size"
         @pagination="getList"
     />
   </div>
