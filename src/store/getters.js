06142d51872694874af98a79547c8a933e4136da
许世飞 <xusf.data4truth.com>
2021-11-04 11:40:02 +0800

    feat(操作日志):权限id和orgId放到请求头

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,14 +1,16 @@
 const getters = {
-  sidebar: (state) => state.app.sidebar,
-  device: (state) => state.app.device,
-  token: (state) => state.user.token,
-  avatar: (state) => state.user.avatar,
-  name: (state) => state.user.name,
-  visitedViews: (state) => state.tagsView.visitedViews,
-  cachedViews: (state) => state.tagsView.cachedViews,
-  // 树状路由
-  permissionTreeRoutes: (state) => state.user.routes,
-  // 树状路由平铺成的list
-  permissionListRoutes: (state) => state.user.routeList
+    sidebar: (state) => state.app.sidebar,
+    device: (state) => state.app.device,
+    token: (state) => state.user.token,
+    avatar: (state) => state.user.avatar,
+    name: (state) => state.user.name,
+    orgId: (state) => state.user.orgId,
+    permissionId: (state) => state.user.permissionId,
+    visitedViews: (state) => state.tagsView.visitedViews,
+    cachedViews: (state) => state.tagsView.cachedViews,
+    // 树状路由
+    permissionTreeRoutes: (state) => state.user.routes,
+    // 树状路由平铺成的list
+    permissionListRoutes: (state) => state.user.routeList
 }
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,71 +1,76 @@
-import { getInfo, login, logout } from '@/api/user'
-import { getToken, removeToken, setToken } from '@/utils/auth'
-import { asyncRoutes, resetRouter } from '@/router'
+import {getInfo, login, logout} from '@/api/user'
+import {getToken, removeToken, setToken} from '@/utils/auth'
+import {asyncRoutes, resetRouter} from '@/router'
 import Layout from '@/layout/index.vue'
-import { isNotEmptyCollection } from '@/utils/common'
-import { CommonEnum } from '@/enums/CommonEnum'
+import {isNotEmptyCollection} from '@/utils/common'
+import {CommonEnum} from '@/enums/CommonEnum'
 
 const getDefaultState = () => {
-  return {
-    token: getToken(),
-    name: '',
-    avatar: '',
-    orgId: '',
-    // 树状路由
-    routes: [],
-    // 树状路由平铺成list
-    routeList: [],
-    addRoutes: []
-  }
+    return {
+        token: getToken(),
+        name: '',
+        avatar: '',
+        orgId: '',
+        //当前页面的id，用来纪录操作日志
+        permissionId: undefined,
+        // 树状路由
+        routes: [],
+        // 树状路由平铺成list
+        routeList: [],
+        addRoutes: []
+    }
 }
 
 const state = getDefaultState()
 
 const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
-  },
-  SET_TOKEN: (state, token) => {
-    state.token = token
-  },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
-  },
-  SET_ORG_ID: (state, orgId) => {
-    state.orgId = orgId
-  },
-  SET_ROUTES: (state, routes) => {
-    state.addRoutes = routes
-    // state.routes = constantRoutes.concat(routes)
-    state.routes = routes
-  },
-  // 保存树状路由平铺成的list
-  SET_ROUTE_LIST: (state, routeList) => {
-    state.routeList = routeList
-  }
+    RESET_STATE: (state) => {
+        Object.assign(state, getDefaultState())
+    },
+    SET_TOKEN: (state, token) => {
+        state.token = token
+    },
+    SET_NAME: (state, name) => {
+        state.name = name
+    },
+    SET_AVATAR: (state, avatar) => {
+        state.avatar = avatar
+    },
+    SET_ORG_ID: (state, orgId) => {
+        state.orgId = orgId
+    },
+    SET_ROUTES: (state, routes) => {
+        state.addRoutes = routes
+        // state.routes = constantRoutes.concat(routes)
+        state.routes = routes
+    },
+    // 保存树状路由平铺成的list
+    SET_ROUTE_LIST: (state, routeList) => {
+        state.routeList = routeList
+    },
+    SET_PERMISSION_ID: (state, permissionId) => {
+        state.permissionId = permissionId
+    }
 }
 
 // 后台返回的权限列表转router格式
 function convertRoute(routes, data) {
-  data.forEach((item) => {
-    const menu = {
-      path: item.path,
-      component: isNotEmptyCollection(item.children)
-        ? Layout
-        : (resolve) => require([`@/views${item.path}`], resolve),
-      name: item.pageName,
-      hidden: item.type === CommonEnum.PERMISSION_MENU ? item.hidden : true, // 是页面的话根据按钮是否隐藏赋值，不是页面的话直接隐藏
-      meta: { title: item.name, icon: item.icon, type: item.type },
-      children: []
-    }
-    if (item.children) {
-      convertRoute(menu.children, item.children)
-    }
-    routes.push(menu)
-  })
+    data.forEach((item) => {
+        const menu = {
+            path: item.path,
+            component: isNotEmptyCollection(item.children)
+                ? Layout
+                : (resolve) => require([`@/views${item.path}`], resolve),
+            name: item.pageName,
+            hidden: item.type === CommonEnum.PERMISSION_MENU ? item.hidden : true, // 是页面的话根据按钮是否隐藏赋值，不是页面的话直接隐藏
+            meta: {title: item.name, icon: item.icon, type: item.type, id: item.id},
+            children: []
+        }
+        if (item.children) {
+            convertRoute(menu.children, item.children)
+        }
+        routes.push(menu)
+    })
 }
 
 /**
@@ -74,102 +79,108 @@ function convertRoute(routes, data) {
  * @param routeList list
  */
 function convertRouteList(treeRoutes, routeList) {
-  treeRoutes.forEach((item) => {
-    if (item.children) {
-      convertRouteList(item.children, routeList)
-    }
-    routeList.push(item)
-  })
+    treeRoutes.forEach((item) => {
+        if (item.children) {
+            convertRouteList(item.children, routeList)
+        }
+        routeList.push(item)
+    })
 }
 
 const actions = {
-  // user login
-  login({ commit }, userInfo) {
-    const { username, password, verifyCode, verifyCodeId, rememberMe } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password, verifyCode, verifyCodeId, rememberMe })
-        .then((response) => {
-          const { data } = response
-          commit('SET_TOKEN', data.token)
-          commit('SET_NAME', data.name)
-          commit('SET_ORG_ID', data.orgId)
-          setToken(data.token)
-          resolve()
+    // user login
+    login({commit}, userInfo) {
+        const {username, password, verifyCode, verifyCodeId, rememberMe} = userInfo
+        return new Promise((resolve, reject) => {
+            login({username: username.trim(), password, verifyCode, verifyCodeId, rememberMe})
+                .then((response) => {
+                    const {data} = response
+                    commit('SET_TOKEN', data.token)
+                    commit('SET_NAME', data.name)
+                    commit('SET_ORG_ID', data.orgId)
+                    setToken(data.token)
+                    resolve()
+                })
+                .catch((error) => {
+                    reject(error)
+                })
         })
-        .catch((error) => {
-          reject(error)
+    },
+
+    // get user info
+    getInfo({commit, state}) {
+        return new Promise((resolve, reject) => {
+            getInfo(state.token)
+                .then((response) => {
+                    const {data} = response
+
+                    if (!data) {
+                        return reject('token验证失败，请尝试重新登录')
+                    }
+
+                    const {name, avatar} = data
+
+                    commit('SET_NAME', name)
+                    commit('SET_AVATAR', avatar)
+
+                    // 根节点不显示
+                    const loadMenuData = response.data.permissionVOS[0].children
+                    // 清空之前的路由
+                    asyncRoutes.length = 0
+                    // 将后台返回的路由转换成vue需要的格式
+                    convertRoute(asyncRoutes, loadMenuData)
+                    // 平铺后的路由
+                    const routeList = []
+                    //树平铺成list，用来判断用户是否有权限
+                    convertRouteList(asyncRoutes, routeList)
+                    const accessedRoutes = asyncRoutes
+                    // 保存vue需要的树状路由
+                    commit('SET_ROUTES', accessedRoutes)
+                    // 保存树状路由平铺成的list
+                    commit('SET_ROUTE_LIST', routeList)
+                    resolve(data)
+                })
+                .catch((error) => {
+                    reject(error)
+                })
         })
-    })
-  },
+    },
 
-  // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token)
-        .then((response) => {
-          const { data } = response
-
-          if (!data) {
-            return reject('token验证失败，请尝试重新登录')
-          }
-
-          const { name, avatar } = data
-
-          commit('SET_NAME', name)
-          commit('SET_AVATAR', avatar)
-
-          // 根节点不显示
-          const loadMenuData = response.data.permissionVOS[0].children
-          // 清空之前的路由
-          asyncRoutes.length = 0
-          // 将后台返回的路由转换成vue需要的格式
-          convertRoute(asyncRoutes, loadMenuData)
-          // 平铺后的路由
-          const routeList = []
-          //树平铺成list，用来判断用户是否有权限
-          convertRouteList(asyncRoutes, routeList)
-          const accessedRoutes = asyncRoutes
-          // 保存vue需要的树状路由
-          commit('SET_ROUTES', accessedRoutes)
-          // 保存树状路由平铺成的list
-          commit('SET_ROUTE_LIST', routeList)
-          resolve(data)
+    // user logout
+    logout({commit, state}) {
+        return new Promise((resolve, reject) => {
+            logout(state.token)
+                .then(() => {
+                    removeToken() // must remove  token  first
+                    resetRouter()
+                    commit('RESET_STATE')
+                    resolve()
+                })
+                .catch((error) => {
+                    reject(error)
+                })
         })
-        .catch((error) => {
-          reject(error)
-        })
-    })
-  },
+    },
 
-  // user logout
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token)
-        .then(() => {
-          removeToken() // must remove  token  first
-          resetRouter()
-          commit('RESET_STATE')
-          resolve()
+    // remove token
+    resetToken({commit}) {
+        return new Promise((resolve) => {
+            removeToken() // must remove  token  first
+            commit('RESET_STATE')
+            resolve()
         })
-        .catch((error) => {
-          reject(error)
+    },
+    //设置当前permissionId，用来记录日志使用
+    setPermissionId({commit, permissionId}) {
+        return new Promise((resolve, reject) => {
+            commit('SET_PERMISSION_ID', permissionId)
         })
-    })
-  },
-
-  // remove token
-  resetToken({ commit }) {
-    return new Promise((resolve) => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
-  }
+    }
 }
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions
+    namespaced: true,
+    state,
+    mutations,
+    actions
 }
