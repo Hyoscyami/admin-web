f8c557eb5b0a9cb1d909ea2528d9473427ee65aa
许世飞 <xusf.data4truth.com>
2021-11-18 16:03:31 +0800

    fix(统计分析):年份重复

--- src/composables/statistics/revoke/index.ts
+++ src/composables/statistics/revoke/index.ts
@@ -1,17 +1,17 @@
-import { reactive, ref } from 'vue'
+import {reactive, ref} from 'vue'
 import {
-  listPreTaxDeductionYears,
-  revokeCount as list,
-  revokeCountExport,
-  totalRevokeCount
+    listPreTaxDeductionYears,
+    revokeCount as list,
+    revokeCountExport,
+    totalRevokeCount
 } from '@/api/statistics/statistics'
-import { SelectGroup, useTable } from '@/model/req/query/Table'
-import { formatYYYY } from '@/utils/time'
-import { listChildrenByCode } from '@/api/sys/dict'
-import { DictEnum } from '@/enums/DictEnum'
-import { DictVO } from '@/model/vo/DictVO'
-import { StatisticVO } from '../../../model/vo/StatisticVO'
-import { QueryStatisticReq, useQueryStatisticReq } from '../../../model/req/query/QueryStatisticReq'
+import {SelectGroup, useTable} from '@/model/req/query/Table'
+import {formatYYYY} from '@/utils/time'
+import {listChildrenByCode} from '@/api/sys/dict'
+import {DictEnum} from '@/enums/DictEnum'
+import {DictVO} from '@/model/vo/DictVO'
+import {StatisticVO} from '../../../model/vo/StatisticVO'
+import {QueryStatisticReq, useQueryStatisticReq} from '../../../model/req/query/QueryStatisticReq'
 import * as echarts from 'echarts'
 
 // 初始化表格的对象
@@ -41,235 +41,235 @@ export const echart = echarts
 
 // 初始化
 export function init() {
-  // 初始化资产类型
-  // listTypes()
-  //初始化年份
-  listYears()
-  // 初始化表格
-  searchFormSubmit()
+    // 初始化资产类型
+    // listTypes()
+    //初始化年份
+    listYears()
+    // 初始化表格
+    searchFormSubmit()
 }
 
 // 搜索机构表单查询
 export function searchFormSubmit() {
-  table.listQuery.page = 1
-  getList()
+    table.listQuery.page = 1
+    getList()
 }
 
 // 获取资产类型下拉框
 export function listTypes() {
-  listChildrenByCode(DictEnum.ASSERT_TYPE).then((response) => {
-    if (table.typesSelect) {
-      table.typesSelect.length = 0
-    }
-    response.data.forEach((item: DictVO) => {
-      const type: SelectGroup = {
-        id: item.id,
-        text: item.name,
-        value: item.id
-      }
-      if (table.typesSelect) {
-        table.typesSelect.push(type)
-      }
+    listChildrenByCode(DictEnum.ASSERT_TYPE).then((response) => {
+        if (table.typesSelect) {
+            table.typesSelect.length = 0
+        }
+        response.data.forEach((item: DictVO) => {
+            const type: SelectGroup = {
+                id: item.id,
+                text: item.name,
+                value: item.id
+            }
+            if (table.typesSelect) {
+                table.typesSelect.push(type)
+            }
+        })
     })
-  })
 }
 
 // 获取可选择年份下拉框
 export function listYears() {
-  listPreTaxDeductionYears().then((response) => {
-    if (table.typesSelect) {
-      table.typesSelect.length = 0
-    }
-    response.data.forEach((item: number) => {
-      const type: SelectGroup = {
-        id: item,
-        text: item.toString(),
-        value: item
-      }
-      if (table.yearsSelect) {
-        table.yearsSelect.push(type)
-      }
+    listPreTaxDeductionYears().then((response) => {
+        if (table.yearsSelect) {
+            table.yearsSelect.length = 0
+        }
+        response.data.forEach((item: number) => {
+            const type: SelectGroup = {
+                id: item,
+                text: item.toString(),
+                value: item
+            }
+            if (table.yearsSelect) {
+                table.yearsSelect.push(type)
+            }
+        })
     })
-  })
 }
 
 // 获取父机构列表数据
 export function getList() {
-  table.listLoading = true
-  if (table.listQuery.orgId) {
-    table.listQuery.orgIds = [table.listQuery.orgId]
-  }
-  list(table.listQuery).then((response) => {
-    table.tableData = response.data.records
-    table.total = response.data.total
-    table.listLoading = false
-  })
-  //刷新表格合计总数
-  getSummaries()
+    table.listLoading = true
+    if (table.listQuery.orgId) {
+        table.listQuery.orgIds = [table.listQuery.orgId]
+    }
+    list(table.listQuery).then((response) => {
+        table.tableData = response.data.records
+        table.total = response.data.total
+        table.listLoading = false
+    })
+    //刷新表格合计总数
+    getSummaries()
 }
 
 // 表格的搜索表单重置
 export function resetSearchForm() {
-  // @ts-ignore
-  searchFormRef.value.searchFormRef.resetFields()
+    // @ts-ignore
+    searchFormRef.value.searchFormRef.resetFields()
 }
 
 // 根据类型刷新表格
 export function filterTableType(data: any) {
-  // 重置查询条件
-  table.listQuery.page = 1
-  if (data.status != undefined && data.status.length === 0) {
+    // 重置查询条件
+    table.listQuery.page = 1
+    if (data.status != undefined && data.status.length === 0) {
+        // @ts-ignore
+        table.listQuery.status.length = 0
+    }
+    if (data.type != undefined && data.type.length === 0) {
+        // @ts-ignore
+        table.listQuery.types.length = 0
+    }
     // @ts-ignore
-    table.listQuery.status.length = 0
-  }
-  if (data.type != undefined && data.type.length === 0) {
+    table.listQuery.status = table.listQuery.status.concat(data.status).filter((item) => item != null)
     // @ts-ignore
-    table.listQuery.types.length = 0
-  }
-  // @ts-ignore
-  table.listQuery.status = table.listQuery.status.concat(data.status).filter((item) => item != null)
-  // @ts-ignore
-  table.listQuery.types = table.listQuery.types.concat(data.type).filter((item) => item != null)
+    table.listQuery.types = table.listQuery.types.concat(data.type).filter((item) => item != null)
 
-  // 刷新表格数据
-  getList()
+    // 刷新表格数据
+    getList()
 }
 
 // 日期转换
 export function formatDate(_row: any, _column: any, cellValue: any): string {
-  return formatYYYY(cellValue)
+    return formatYYYY(cellValue)
 }
 
 // 比例显示
 export function formatProportion(_row: any, _column: any, cellValue: any): string {
-  return cellValue + '%'
+    return cellValue + '%'
 }
 
 //表格合计行
 function getSummaries() {
-  totalRevokeCount(table.listQuery).then((response) => {
-    tableTotal.value = ['合计']
-    tableTotal.value.push(response.data.revokeCount)
-    tableTotal.value.push(response.data.revokeAmount)
-    tableTotal.value.push(response.data.revokedCount)
-    tableTotal.value.push(response.data.revokedAmount)
-    tableTotal.value.push(response.data.revokedProportion + '%')
-    tableTotal.value.push(response.data.unRevokeCount)
-    tableTotal.value.push(response.data.unRevokeAmount)
-    if (echartsCountData.value.length > 0) {
-      echartsCountData.value.length = 0
-    }
-    if (echartsAmountData.value.length > 0) {
-      echartsAmountData.value.length = 0
-    }
-    //初始化echarts
-    echartsCountData.value.push({
-      value: response.data.revokedCount,
-      name: '核销收回笔数'
+    totalRevokeCount(table.listQuery).then((response) => {
+        tableTotal.value = ['合计']
+        tableTotal.value.push(response.data.revokeCount)
+        tableTotal.value.push(response.data.revokeAmount)
+        tableTotal.value.push(response.data.revokedCount)
+        tableTotal.value.push(response.data.revokedAmount)
+        tableTotal.value.push(response.data.revokedProportion + '%')
+        tableTotal.value.push(response.data.unRevokeCount)
+        tableTotal.value.push(response.data.unRevokeAmount)
+        if (echartsCountData.value.length > 0) {
+            echartsCountData.value.length = 0
+        }
+        if (echartsAmountData.value.length > 0) {
+            echartsAmountData.value.length = 0
+        }
+        //初始化echarts
+        echartsCountData.value.push({
+            value: response.data.revokedCount,
+            name: '核销收回笔数'
+        })
+        echartsCountData.value.push({
+            value: response.data.unRevokeCount,
+            name: '核销未收回笔数'
+        })
+        echartsAmountData.value.push({
+            value: response.data.revokeAmount,
+            name: '核销收回金额'
+        })
+        echartsAmountData.value.push({
+            value: response.data.unRevokeAmount,
+            name: '核销未收回金额'
+        })
+        // 初始化echarts
+        initEcharts()
     })
-    echartsCountData.value.push({
-      value: response.data.unRevokeCount,
-      name: '核销未收回笔数'
-    })
-    echartsAmountData.value.push({
-      value: response.data.revokeAmount,
-      name: '核销收回金额'
-    })
-    echartsAmountData.value.push({
-      value: response.data.unRevokeAmount,
-      name: '核销未收回金额'
-    })
-    // 初始化echarts
-    initEcharts()
-  })
 }
 
 //表格合计行
 export function getTableTotal() {
-  return tableTotal.value
+    return tableTotal.value
 }
 
 //导出
 export function exportList() {
-  exportLoading.value = true
-  revokeCountExport(table.listQuery).then((response) => {
-    window.open(response.data)
-    exportLoading.value = false
-  })
+    exportLoading.value = true
+    revokeCountExport(table.listQuery).then((response) => {
+        window.open(response.data)
+        exportLoading.value = false
+    })
 }
 
 //初始化echarts
 export function initEcharts() {
-  type EChartsOption = echarts.EChartsOption
+    type EChartsOption = echarts.EChartsOption
 
-  // @ts-ignore
-  const myChartCount = echart.init(document.getElementById('echartCount'))
-  // @ts-ignore
-  const myChartAmount = echart.init(document.getElementById('echartAmount'))
-  //笔数
-  let countOption: EChartsOption
-  //金额
-  let amountOption: EChartsOption
+    // @ts-ignore
+    const myChartCount = echart.init(document.getElementById('echartCount'))
+    // @ts-ignore
+    const myChartAmount = echart.init(document.getElementById('echartAmount'))
+    //笔数
+    let countOption: EChartsOption
+    //金额
+    let amountOption: EChartsOption
 
-  countOption = reactive({
-    title: {
-      text: '收回税前扣除情况笔数分析',
-      subtext: '',
-      left: 'center'
-    },
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      orient: 'vertical',
-      left: 'left'
-    },
-    series: [
-      {
-        name: '',
-        type: 'pie',
-        radius: '50%',
-        data: echartsCountData.value,
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
-      }
-    ]
-  })
-  amountOption = reactive({
-    title: {
-      text: '收回税前扣除金额分析',
-      subtext: '',
-      left: 'center'
-    },
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      orient: 'vertical',
-      left: 'left'
-    },
-    series: [
-      {
-        name: '',
-        type: 'pie',
-        radius: '50%',
-        data: echartsAmountData.value,
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
-      }
-    ]
-  })
+    countOption = reactive({
+        title: {
+            text: '收回税前扣除情况笔数分析',
+            subtext: '',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: '',
+                type: 'pie',
+                radius: '50%',
+                data: echartsCountData.value,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    })
+    amountOption = reactive({
+        title: {
+            text: '收回税前扣除金额分析',
+            subtext: '',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: '',
+                type: 'pie',
+                radius: '50%',
+                data: echartsAmountData.value,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    })
 
-  myChartCount.setOption(countOption)
-  myChartAmount.setOption(amountOption)
+    myChartCount.setOption(countOption)
+    myChartAmount.setOption(amountOption)
 }
--- src/composables/statistics/write-off-revoke/index.ts
+++ src/composables/statistics/write-off-revoke/index.ts
@@ -1,19 +1,19 @@
-import { reactive, ref } from 'vue'
+import {reactive, ref} from 'vue'
 import {
-  listPreTaxDeductionYears,
-  totalWriteOffRevokeCount,
-  writeOffRevokeCount as list,
-  writeOffRevokeCountExport
+    listPreTaxDeductionYears,
+    totalWriteOffRevokeCount,
+    writeOffRevokeCount as list,
+    writeOffRevokeCountExport
 } from '@/api/statistics/statistics'
-import { SelectGroup, useTable } from '@/model/req/query/Table'
-import { formatYYYY } from '@/utils/time'
-import { listChildrenByCode } from '@/api/sys/dict'
-import { DictEnum } from '@/enums/DictEnum'
-import { DictVO } from '@/model/vo/DictVO'
-import { StatisticVO } from '../../../model/vo/StatisticVO'
-import { QueryStatisticReq, useQueryStatisticReq } from '../../../model/req/query/QueryStatisticReq'
-import { entityList } from '../../../api/sys/org'
-import { useQueryOrgReq } from '../../../model/req/query/QueryOrgReq'
+import {SelectGroup, useTable} from '@/model/req/query/Table'
+import {formatYYYY} from '@/utils/time'
+import {listChildrenByCode} from '@/api/sys/dict'
+import {DictEnum} from '@/enums/DictEnum'
+import {DictVO} from '@/model/vo/DictVO'
+import {StatisticVO} from '../../../model/vo/StatisticVO'
+import {QueryStatisticReq, useQueryStatisticReq} from '../../../model/req/query/QueryStatisticReq'
+import {entityList} from '../../../api/sys/org'
+import {useQueryOrgReq} from '../../../model/req/query/QueryOrgReq'
 import * as echarts from 'echarts'
 
 // 初始化表格的对象
@@ -43,265 +43,265 @@ export const echart = echarts
 
 // 初始化
 export function init() {
-  // 初始化资产类型
-  // listTypes()
-  //初始化年份
-  listYears()
-  //初始化组织
-  listOrgs()
-  // 初始化表格
-  searchFormSubmit()
+    // 初始化资产类型
+    // listTypes()
+    //初始化年份
+    listYears()
+    //初始化组织
+    listOrgs()
+    // 初始化表格
+    searchFormSubmit()
 }
 
 // 搜索机构表单查询
 export function searchFormSubmit() {
-  table.listQuery.page = 1
-  getList()
+    table.listQuery.page = 1
+    getList()
 }
 
 // 获取资产类型下拉框
 export function listTypes() {
-  listChildrenByCode(DictEnum.ASSERT_TYPE).then((response) => {
-    if (table.typesSelect) {
-      table.typesSelect.length = 0
-    }
-    response.data.forEach((item: DictVO) => {
-      const type: SelectGroup = {
-        id: item.id,
-        text: item.name,
-        value: item.id
-      }
-      if (table.typesSelect) {
-        table.typesSelect.push(type)
-      }
+    listChildrenByCode(DictEnum.ASSERT_TYPE).then((response) => {
+        if (table.typesSelect) {
+            table.typesSelect.length = 0
+        }
+        response.data.forEach((item: DictVO) => {
+            const type: SelectGroup = {
+                id: item.id,
+                text: item.name,
+                value: item.id
+            }
+            if (table.typesSelect) {
+                table.typesSelect.push(type)
+            }
+        })
     })
-  })
 }
 
 // 获取组织下拉框
 export function listOrgs() {
-  entityList(useQueryOrgReq(20)).then((response) => {
-    if (table.orgsSelect) {
-      table.orgsSelect.length = 0
-    }
-    response.data.forEach((item: DictVO) => {
-      const type: SelectGroup = {
-        id: item.id,
-        text: item.name,
-        value: item.id
-      }
-      if (table.orgsSelect) {
-        table.orgsSelect.push(type)
-      }
+    entityList(useQueryOrgReq(20)).then((response) => {
+        if (table.orgsSelect) {
+            table.orgsSelect.length = 0
+        }
+        response.data.forEach((item: DictVO) => {
+            const type: SelectGroup = {
+                id: item.id,
+                text: item.name,
+                value: item.id
+            }
+            if (table.orgsSelect) {
+                table.orgsSelect.push(type)
+            }
+        })
     })
-  })
 }
 
 // 获取可选择年份下拉框
 export function listYears() {
-  listPreTaxDeductionYears().then((response) => {
-    if (table.typesSelect) {
-      table.typesSelect.length = 0
-    }
-    response.data.forEach((item: number) => {
-      const type: SelectGroup = {
-        id: item,
-        text: item.toString(),
-        value: item
-      }
-      if (table.yearsSelect) {
-        table.yearsSelect.push(type)
-      }
+    listPreTaxDeductionYears().then((response) => {
+        if (table.yearsSelect) {
+            table.yearsSelect.length = 0
+        }
+        response.data.forEach((item: number) => {
+            const type: SelectGroup = {
+                id: item,
+                text: item.toString(),
+                value: item
+            }
+            if (table.yearsSelect) {
+                table.yearsSelect.push(type)
+            }
+        })
     })
-  })
 }
 
 // 获取父机构列表数据
 export function getList() {
-  table.listLoading = true
-  if (table.listQuery.orgId) {
-    table.listQuery.orgIds = [table.listQuery.orgId]
-  }
-  list(table.listQuery).then((response) => {
-    table.tableData = response.data.records
-    table.total = response.data.total
-    table.listLoading = false
-  })
-  //刷新表格合计总数
-  getSummaries()
+    table.listLoading = true
+    if (table.listQuery.orgId) {
+        table.listQuery.orgIds = [table.listQuery.orgId]
+    }
+    list(table.listQuery).then((response) => {
+        table.tableData = response.data.records
+        table.total = response.data.total
+        table.listLoading = false
+    })
+    //刷新表格合计总数
+    getSummaries()
 }
 
 // 表格的搜索表单重置
 export function resetSearchForm() {
-  // @ts-ignore
-  searchFormRef.value.searchFormRef.resetFields()
+    // @ts-ignore
+    searchFormRef.value.searchFormRef.resetFields()
 }
 
 // 根据类型刷新表格
 export function filterTableType(data: any) {
-  // 重置查询条件
-  table.listQuery.page = 1
-  if (data.status != undefined && data.status.length === 0) {
+    // 重置查询条件
+    table.listQuery.page = 1
+    if (data.status != undefined && data.status.length === 0) {
+        // @ts-ignore
+        table.listQuery.status.length = 0
+    }
+    if (data.type != undefined && data.type.length === 0) {
+        // @ts-ignore
+        table.listQuery.types.length = 0
+    }
+    console.log('data:', data)
     // @ts-ignore
-    table.listQuery.status.length = 0
-  }
-  if (data.type != undefined && data.type.length === 0) {
+    table.listQuery.status = table.listQuery.status.concat(data.status).filter((item) => item != null)
     // @ts-ignore
-    table.listQuery.types.length = 0
-  }
-  console.log('data:', data)
-  // @ts-ignore
-  table.listQuery.status = table.listQuery.status.concat(data.status).filter((item) => item != null)
-  // @ts-ignore
-  table.listQuery.types = table.listQuery.types.concat(data.type).filter((item) => item != null)
+    table.listQuery.types = table.listQuery.types.concat(data.type).filter((item) => item != null)
 
-  // 刷新表格数据
-  getList()
+    // 刷新表格数据
+    getList()
 }
 
 // 日期转换
 export function formatDate(_row: any, _column: any, cellValue: any): string {
-  return formatYYYY(cellValue)
+    return formatYYYY(cellValue)
 }
 
 // 比例显示
 export function formatProportion(_row: any, _column: any, cellValue: any): string {
-  return cellValue + '%'
+    return cellValue + '%'
 }
 
 //表格合计行
 function getSummaries() {
-  totalWriteOffRevokeCount(table.listQuery).then((response) => {
-    tableTotal.value = ['合计']
-    tableTotal.value.push(response.data.writtenOffCount)
-    tableTotal.value.push(response.data.writtenOffAmount)
-    tableTotal.value.push(response.data.revokeCount)
-    tableTotal.value.push(response.data.revokeAmount)
-    tableTotal.value.push(response.data.revokedProportion + '%')
-    tableTotal.value.push(response.data.unRevokeCount)
-    tableTotal.value.push(response.data.unRevokeAmount)
-    if (echartsCountData.value.length > 0) {
-      echartsCountData.value.length = 0
-    }
-    if (echartsAmountData.value.length > 0) {
-      echartsAmountData.value.length = 0
-    }
-    //初始化echarts
-    echartsCountData.value.push({
-      value: response.data.writtenOffCount,
-      name: '已核销呆账笔数'
+    totalWriteOffRevokeCount(table.listQuery).then((response) => {
+        tableTotal.value = ['合计']
+        tableTotal.value.push(response.data.writtenOffCount)
+        tableTotal.value.push(response.data.writtenOffAmount)
+        tableTotal.value.push(response.data.revokeCount)
+        tableTotal.value.push(response.data.revokeAmount)
+        tableTotal.value.push(response.data.revokedProportion + '%')
+        tableTotal.value.push(response.data.unRevokeCount)
+        tableTotal.value.push(response.data.unRevokeAmount)
+        if (echartsCountData.value.length > 0) {
+            echartsCountData.value.length = 0
+        }
+        if (echartsAmountData.value.length > 0) {
+            echartsAmountData.value.length = 0
+        }
+        //初始化echarts
+        echartsCountData.value.push({
+            value: response.data.writtenOffCount,
+            name: '已核销呆账笔数'
+        })
+        echartsCountData.value.push({
+            value: response.data.revokeCount,
+            name: '核销收回笔数'
+        })
+        echartsCountData.value.push({
+            value: response.data.unRevokeCount,
+            name: '核销未收回笔数'
+        })
+        echartsAmountData.value.push({
+            value: response.data.writtenOffAmount,
+            name: '已核销呆账金额'
+        })
+        echartsAmountData.value.push({
+            value: response.data.revokeAmount,
+            name: '核销收回金额'
+        })
+        echartsAmountData.value.push({
+            value: response.data.unRevokeAmount,
+            name: '核销未收回金额'
+        })
+        // 初始化echarts
+        initEcharts()
     })
-    echartsCountData.value.push({
-      value: response.data.revokeCount,
-      name: '核销收回笔数'
-    })
-    echartsCountData.value.push({
-      value: response.data.unRevokeCount,
-      name: '核销未收回笔数'
-    })
-    echartsAmountData.value.push({
-      value: response.data.writtenOffAmount,
-      name: '已核销呆账金额'
-    })
-    echartsAmountData.value.push({
-      value: response.data.revokeAmount,
-      name: '核销收回金额'
-    })
-    echartsAmountData.value.push({
-      value: response.data.unRevokeAmount,
-      name: '核销未收回金额'
-    })
-    // 初始化echarts
-    initEcharts()
-  })
 }
 
 //表格合计行
 export function getTableTotal() {
-  return tableTotal.value
+    return tableTotal.value
 }
 
 //导出
 export function exportList() {
-  exportLoading.value = true
-  writeOffRevokeCountExport(table.listQuery).then((response) => {
-    window.open(response.data)
-    exportLoading.value = false
-  })
+    exportLoading.value = true
+    writeOffRevokeCountExport(table.listQuery).then((response) => {
+        window.open(response.data)
+        exportLoading.value = false
+    })
 }
 
 //初始化echarts
 export function initEcharts() {
-  type EChartsOption = echarts.EChartsOption
+    type EChartsOption = echarts.EChartsOption
 
-  // @ts-ignore
-  const myChartCount = echart.init(document.getElementById('echartCount'))
-  // @ts-ignore
-  const myChartAmount = echart.init(document.getElementById('echartAmount'))
-  //笔数
-  let countOption: EChartsOption
-  //金额
-  let amountOption: EChartsOption
+    // @ts-ignore
+    const myChartCount = echart.init(document.getElementById('echartCount'))
+    // @ts-ignore
+    const myChartAmount = echart.init(document.getElementById('echartAmount'))
+    //笔数
+    let countOption: EChartsOption
+    //金额
+    let amountOption: EChartsOption
 
-  countOption = reactive({
-    title: {
-      text: '核销收回情况笔数分析',
-      subtext: '',
-      left: 'center'
-    },
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      orient: 'vertical',
-      left: 'left'
-    },
-    series: [
-      {
-        name: '',
-        type: 'pie',
-        radius: '50%',
-        data: echartsCountData.value,
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
-      }
-    ]
-  })
-  amountOption = reactive({
-    title: {
-      text: '核销收回情况金额分析',
-      subtext: '',
-      left: 'center'
-    },
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      orient: 'vertical',
-      left: 'left'
-    },
-    series: [
-      {
-        name: '',
-        type: 'pie',
-        radius: '50%',
-        data: echartsAmountData.value,
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
-      }
-    ]
-  })
+    countOption = reactive({
+        title: {
+            text: '核销收回情况笔数分析',
+            subtext: '',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: '',
+                type: 'pie',
+                radius: '50%',
+                data: echartsCountData.value,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    })
+    amountOption = reactive({
+        title: {
+            text: '核销收回情况金额分析',
+            subtext: '',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+                name: '',
+                type: 'pie',
+                radius: '50%',
+                data: echartsAmountData.value,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    })
 
-  myChartCount.setOption(countOption)
-  myChartAmount.setOption(amountOption)
+    myChartCount.setOption(countOption)
+    myChartAmount.setOption(amountOption)
 }
