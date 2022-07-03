88aa9ba2918f21463508405751324eb77f3e7769
xushifei <xsfzj.2009@qq.com>
2022-07-03 15:32:30 +0800

    feat(头像)：头像替换成本地资源

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -11,7 +11,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar + '?imageView2/1/w/80/h/80'" class="user-avatar"/>
+          <img src="@/assets/avatar.gif?imageView2/1/w/80/h/80'" class="user-avatar"/>
           <i class="el-icon-arrow-down el-icon--right"/>
         </div>
         <template #dropdown>
